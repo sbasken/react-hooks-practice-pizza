3a2afc55b3f5fe67c086f5ad0959ebea97944c35
Saki <sbasken@gmail.com>
2023-02-28 13:58:06 -0800

    render pizzas after fetching from API server

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import PizzaForm from "./PizzaForm";
 import PizzaList from "./PizzaList";
 
 function App() {
+  const [ pizzas, setPizzas ] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pizzas")
+      .then(res => res.json())
+      .then(data => setPizzas(data))
+  }, [])
+
   return (
     <>
       <Header />
       <PizzaForm />
-      <PizzaList />
+      <PizzaList pizzas={pizzas}/>
     </>
   );
 }
--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Pizza from "./Pizza";
 
-function PizzaList() {
+function PizzaList({ pizzas }) {
+  console.log(pizzas)
   return (
     <table className="table table-striped">
       <thead>
@@ -14,7 +15,7 @@ function PizzaList() {
       </thead>
       <tbody>
         {
-          //render Pizza here
+         pizzas.map( pizza => <Pizza id={pizza.id} pizza={pizza}/>)
         }
       </tbody>
     </table>
